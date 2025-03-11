e76dd911efdfaace0fd5fc93614ddee0e145624b
Sarah Shook <srowlesdeveloper@gmail.com>
2025-03-11 11:56:28 -0700

    css

---
+++ components/global/Button.vue
@@ -0,0 +1,20 @@
+<template>
+	<button
+		v-if="text"
+		class="btn"
+		:class="{ secondary: variant === 'secondary' }"
+	>
+		{{ text }}
+	</button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+	text: string;
+	variant?: string;
+}
+
+defineProps<Props>();
+</script>
+
+<style lang="scss"></style>
