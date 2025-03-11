066640079157b71635a81bf582f9ffe8e979c0da
Sarah Shook <srowlesdeveloper@gmail.com>
2025-03-11 14:48:17 -0700

    player

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,23 @@
 <template>
 	<div class="home">
-		<Button text="Start New Game" @click="handleStartGame" />
-		<Button text="Join Game" @click="handleJoinGame"></Button>
+		<Button
+			v-if="showStartGameButton"
+			text="Start New Game"
+			@click="handleStartGame"
+		/>
+		<Button v-else text="Join Game" @click="handleJoinGame"></Button>
 	</div>
 </template>
 <script setup lang="ts">
 import Button from "~/components/global/Button.vue";
 import { buildGameObject } from "~/server/src/activities";
+
+const router = useRouter();
+
+const player1 = ref(false);
+const player2 = ref(false);
+
+const showStartGameButton = ref(true);
 const handleStartGame = async () => {
 	// console.log("HELLO", worker);
 	// const res = await $fetch("/api/temporal");
@@ -14,7 +25,17 @@ const handleStartGame = async () => {
 	// return res;
 	const game = await buildGameObject();
 	console.log("GAME", game);
+
+	router.push(game.gameID.toString());
+
+	player1.value = true;
 };
+
+watchEffect(() => {
+	if (player1.value) {
+		showStartGameButton.value = false;
+	}
+});
 
 onMounted(async () => {
 	const res = await $fetch("/api/temporal");
