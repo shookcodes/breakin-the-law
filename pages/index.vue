48b41d0bb78c1de55a907ec30c1ce87a467a8a48
Sarah Shook <srowlesdeveloper@gmail.com>
2025-03-11 12:30:33 -0700

    svg

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
-	<div><Button text="Start New Game" /> <Button text="Join Game"></Button></div>
+	<div>
+		<Button text="Start New Game" @click="handleStartGame" />
+		<Button text="Join Game" @click="handleJoinGame"></Button>
+	</div>
 </template>
 <script setup lang="ts">
 import Button from "~/components/global/Button.vue";
+
+const handleStartGame = async () => {
+	console.log("HELLO");
+};
+
+const handleJoinGame = async () => {};
 </script>
